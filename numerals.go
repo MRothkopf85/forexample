a4c8e49a4898929a5ea9ea6088957bc71f83a833
Matt Rothkopf <mattrothkopf@localhost.localdomain>
2019-07-13 01:29:31 -0400

    updated code

--- numerals.go
+++ numerals.go
@@ -5,5 +5,18 @@ import "fmt"
 func main() {
 	n := 64
 
-	fmt.Printf("%d\n %b\n %X", n, n, n)
+	fmt.Printf("%d\n %b\n %X \n\n", n, n, n)
+
+	i := 42
+
+	fmt.Printf("%d\n %b\n %X \n\n", i, i, i)
+
+	f := i << 1
+
+	fmt.Printf("%d\n %b\n %X \n\n", f, f, f)
+
+	a := `Mike said, "Hello"`
+
+	fmt.Println(a)
+
 }
